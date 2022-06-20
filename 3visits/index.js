32bca5890e8cd8f97bbe2edc4697273889fd973a
Ubuntu <ubuntu@ip-172-31-8-121.ap-northeast-2.compute.internal>
2022-06-20 08:40:16 +0000

    Finish redis-server config

--- 3visits/index.js
+++ 3visits/index.js
@@ -2,26 +2,37 @@ const express = require('express');
 const { createClient } = require('redis');
 
 const app = express();
-const client = createClient({
-  host: 'redis-server',
-  port: 6379,
-});
 
-app.get('/', async (req, res) => {
+const getRedisClient = async () => {
+  const client = createClient({
+    url: process.env.REDIS_URL,
+  });
+
   client.on('error', (err) => console.log('Redis Client Error', err));
   await client.connect();
 
+  return client;
+};
+
+let redisClient;
+app.get('/', async (req, res) => {
   try {
-    const visits = (await client.get('visits')) || 1;
+    if (!redisClient) {
+      redisClient = await getRedisClient();
+    }
+
+    const visits = (await redisClient.get('visits')) || 1;
 
     res.send(`Number of visits: ${visits}`);
 
-    await client.set('visits', parseInt(visits) + 1);
+    await redisClient.set('visits', parseInt(visits) + 1);
   } catch (e) {
     res.send(`There was an error: ${e}`);
   }
 });
 
 app.listen('5000', () => {
-  console.log('app listening on http://34.64.252.61:5000');
+  console.log(
+    'app listening on http://ec2-52-79-62-94.ap-northeast-2.compute.amazonaws.com:5000'
+  );
 });
