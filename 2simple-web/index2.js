9749ec296c98df13d45be3119b869484577befda
z3zzz <zabra7946@naver.com>
2022-07-06 00:38:32 +0000

    Update

--- 2simple-web/index2.js
+++ 2simple-web/index2.js
@@ -3,11 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Node1 there')
+    res.send('Node2 there')
 })
 
 app.listen('5000', () => {
   console.log(
-    'app listening on http://ec2-52-79-62-94.ap-northeast-2.compute.amazonaws.com:5000'
+    'app listening on http://ec2-52-79-62-94.ap-northeast-2.compute.amazonaws.com:3000'
   );
 });
